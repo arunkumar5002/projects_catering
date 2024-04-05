57c2c6165ca82f5eb59f536f907f55819dfef174
arunkumar <arunaddobyte@gmail.com>
2024-04-05 17:14:11 +0530

    2nd commit

--- application/views/web/template.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $this->load->view($theme . '/includes/leftmenu');
-    $this->load->view($theme . '/includes/header');
-    $this->load->view($theme . '/modules/' . $module . '/' . $page);
-    $this->load->view($theme . '/includes/footer');
-?>
